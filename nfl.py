2157d5db906bfddea8d0c2430bead50bb4ea339b
Samuel Wesley <swcprog@gmail.com>
2022-09-09 14:49:04 -0300

    teste

--- nfl.py
+++ nfl.py
@@ -1,21 +1,45 @@
 
-from cmath import log
+
+import json
+import time
+from pydoc import html
+
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-import json
-import time
+from selenium.webdriver.firefox.options import Options
 
 URL = 'https://www.nfl.com/stats/player-stats/'
 option = Options()
+option.headless = True
 driver = webdriver.Firefox()
 driver.get(URL)
-time.sleep(3)
+time.sleep(10)
 
 element = driver.find_element(By.CLASS_NAME, "d3-o-dropdown")
-element.send_keys("2021")
-time.sleep(3)
+element.send_keys('2021')
+time.sleep(5)
+tb = driver.find_element(By.TAG_NAME, "table").get_attribute('outerHTML')
+
+#pegando todos os elementos com essa classe.
+img = driver.find_elements(By.CLASS_NAME, "img-responsive")
+img_src = []
+soup = BeautifulSoup(tb, "html.parser")
+df_full = pd.read_html(str(soup))[0]
+
+df = df_full[["Player", "Pass Yds"]]
+
+
+contador = 0
+for i in img[1:26]:
+    img_src.append((i.get_attribute("src")))
+    print(i)
+
+
+    
+
+
+
 
 driver.quit()
